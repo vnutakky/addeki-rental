479f7bdf959fc30b512d81f4f094088d6266217f
vnutakky <vnutakky@gmail.com>
2022-02-28 11:24:17 -0500

    header component added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {Switch,Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import AccessPage from './pages/Access/access.component';
+import Header from './components/header/header-component.jsx'
 
 function App() {
   return (
     <div >
+    <Header/>
     <Switch>
     <Route exact path='/'component={HomePage} />
     <Route path='/access'component={AccessPage} />
